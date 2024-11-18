798a6a19e9415f2af3d533b6289ea4befa93f332
levchuk <swankyyy1@gmail.com>
2024-11-18 22:42:41 +0300

    add new endpoint for creating new brand

--- app/api/products/views.py
+++ app/api/products/views.py
@@ -12,15 +12,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 router = APIRouter()
 
 
-
 @router.post('/add_new_brand', status_code=status.HTTP_201_CREATED, response_model=BrandBase,
              summary="Add a new brand of sneakers")
-async def add_brand(new_brand: str,  session: AsyncSession = Depends(db_config.get_session)):
+async def add_brand(new_brand: str, session: AsyncSession = Depends(db_config.get_session)):
     """Takes name of brand(str) and create new brand"""
     response = await _add_brand(new_brand=new_brand, session=session)
     if response:
         return response
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
 
 @router.post('/add_new_size', status_code=status.HTTP_201_CREATED, response_model=SizeBase,
              summary="Add a new size of sneaker")
