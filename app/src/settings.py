38d2d9bd98bf03b8557da52422cb4bb470c30a2d
levchuk <swankyyy1@gmail.com>
2024-12-10 16:44:41 +0300

    solved problem with python-multipart solved problem with image path

--- app/src/settings.py
+++ app/src/settings.py
@@ -1,7 +1,7 @@
 class Settings:
     DB_URL = 'postgresql+asyncpg://db:db@localhost:5432/db'
     DB_ECHO = False
-    STORAGE_URL = '../app/images'
+    STORAGE_URL = 'images'
 
 
 settings = Settings()
