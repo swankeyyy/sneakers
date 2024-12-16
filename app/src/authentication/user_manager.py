289f59219c73ce7a18a1007d101e3e159da23294
levchuk <swankyyy1@gmail.com>
2024-12-16 12:45:22 +0300

    add password validator for user

--- app/src/authentication/user_manager.py
+++ app/src/authentication/user_manager.py
@@ -1,9 +1,10 @@
 import uuid
-from typing import Optional
+from typing import Optional, Union
 
-from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi import Request
+from fastapi_users import BaseUserManager, UUIDIDMixin, InvalidPasswordException
 
+from api.users.schemas import UserCreate
 from app.src.models.user import User
 from app.src.settings import settings
 
@@ -13,6 +14,20 @@ SECRET = settings.SECRET_KEY_USERS
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
+
+    async def validate_password(
+            self,
+            password: str,
+            user: Union[UserCreate, User],
+    ) -> None:
+        if len(password) < 8:
+            raise InvalidPasswordException(
+                reason="Password should be at least 8 characters"
+            )
+        if user.email in password:
+            raise InvalidPasswordException(
+                reason="Password should not contain e-mail"
+            )
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
